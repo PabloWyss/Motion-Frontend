cb4da1478a2ad1d9964aa235e3eb2e4cc95d4e95
Enrico Giangrossi <enrico@giangrossi.de>
2023-02-27 13:07:16 +0100

    Added Axios, basic login

--- src/Components/AuthComp/SignInRight/SignInRight.js
+++ src/Components/AuthComp/SignInRight/SignInRight.js
@@ -1,5 +1,51 @@
+import { useState } from "react";
+import callAPI from "../../../axios/auth";
+import { useNavigate } from "react-router-dom";
+
 function SignInRight() {
-  return <p>Sign In</p>;
+    const [userEmail, setEmail] = useState("");
+    const [userPassword, setPassword] = useState("");
+    const navigate = useNavigate();
+  
+    //store typed email
+    const handleEmailInput = (e) => {
+      setEmail(e.target.value);
+    };
+  
+    //store typed password
+    const handlePasswordInput = (e) => {
+      setPassword(e.target.value);
+    };
+  
+    //login
+  const handleLoginClick = async (e) => {
+    e.preventDefault();
+    //registration request to API
+    const response = await callAPI.post(
+      "token/",
+      JSON.stringify({
+        email: userEmail,
+        password: userPassword,
+      })
+    );
+    localStorage.setItem('auth-token', response.data.access);
+    //redirect to homepage
+    navigate("/");
+  };
+
+
+  return (
+    <form className="login-form">
+      <input placeholder="Email" type="email" value={userEmail} onChange={handleEmailInput} />
+      <input
+        placeholder="Password"
+        type="password"
+        value={userPassword}
+        onChange={handlePasswordInput}
+      />
+      <button onClick={handleLoginClick}>Login</button>
+    </form>
+  );
 }
 
 export default SignInRight;
