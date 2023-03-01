3b88f9f66d77872a002b315cbd7112149140b150
Pablo Wyss <pabloawyss@gmail.com>
2023-03-01 11:49:44 +0100

    nav bar navigation of 3 buttons added

--- src/Components/Navigation/NavigationDots/navigationDots.js
+++ src/Components/Navigation/NavigationDots/navigationDots.js
@@ -3,16 +3,27 @@ import logoutImage from "../../../assets/svgs/ShapeLogout.svg"
 import { NavigationDotsUl,
     NavigationDotsLi,
     NavigationDotsImg} from "../navigation.style"
+import { useNavigate } from "react-router-dom";
 
 const NavigationDots = () => {
 
+    const navigate = useNavigate()
+    const handleClickProfile = () => {
+        navigate("/profile")
+    }
+
+    const handleClickLogout = () => {
+        localStorage.removeItem("auth-token")
+        navigate("/signin")
+    }
+
     return (
         <NavigationDotsUl>
-            <NavigationDotsLi>
+            <NavigationDotsLi onClick={handleClickProfile}>
                 <NavigationDotsImg alt="profileLogo Image" src={profileImage}/>
                 <p>Profile</p>
             </NavigationDotsLi>
-            <NavigationDotsLi>
+            <NavigationDotsLi onClick={handleClickLogout}>
                 <NavigationDotsImg alt="Logout Image" src={logoutImage}/>
                 <p>Logout</p>
             </NavigationDotsLi>
