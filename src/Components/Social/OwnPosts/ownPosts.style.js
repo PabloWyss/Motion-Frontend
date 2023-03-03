eb9394442016ed7f5352ebc7117b20278ed9466f
Peevee66 <peter.vajk@gmail.com>
2023-03-03 10:54:43 +0100

    create post implementet without styling

--- src/Components/Social/OwnPosts/ownPosts.style.js
+++ src/Components/Social/OwnPosts/ownPosts.style.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const MainContainer = styled.div`
   width: 100%;
+  max-width: 30rem;
   background: #ffffff;
   box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.2), 0px 10px 20px rgba(0, 0, 0, 0.05);
   border-radius: 4px;
